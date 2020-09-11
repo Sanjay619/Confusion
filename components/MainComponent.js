48e75044c2b84d70295eb078c14bca4ac7d361f8
sanjay619 <Sanjaigoswami619@gmail.com>
2020-09-11 18:52:21 +0500

    Components Part 2

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,29 +1,54 @@
 import React, {Component} from 'react';
 import Menu from './MenuComponent';
+import DishDetail from './DishdetailComponent'
 import { DISHES } from '../shared/dishes';
-import { Dimensions, View } from 'react-native';
+import HomeComponent from './HomeComponent'
+import { createStackNavigator  } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import Dishdetail from './DishdetailComponent';
 
+
+const  Stack = createStackNavigator();
+
+function MyStack (){
+    return(
+        <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home" >
+            <Stack.Screen name = "Home" component = {HomeComponent} />
+            <Stack.Screen name = "Menu" component = {Menu} />
+            <Stack.Screen name = "Dish Detail" component = {Dishdetail} />
+         </Stack.Navigator>{}
+         </NavigationContainer>
+    )
+}
+
+const Drawer = createDrawerNavigator();
+
+    function MyDrawerNavigator(){
+        return(
+            <NavigationContainer>
+            <Drawer.Navigator initialRouteName="Home" >
+                <Drawer.Screen name = "Home" component = {HomeComponent} />
+                <Drawer.Screen name = "Menu" component = {Menu} />
+                <Drawer.Screen name = "Dish Detail" component = {Dishdetail} />
+             </Drawer.Navigator>{}
+             </NavigationContainer>
+        )
+    }
 class Main extends Component {
     constructor(props){
         super(props);
-        this.state = {
-            dishes :  DISHES,
-            selectedDish : null
-        };
+    
     }
 
-    onDishSelected(dishId){
-            this.setState({selectedDish : dishId})
-    }
+ 
     render(){
         return(
-            <View style={{flex : 1}}>
-                <Menu dishes = {this.state.dishes} onPress={(dishId) => this.onDishSelected(dishId)} />
-            <Dishdetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish) [0]}/>
+        
+             <MyDrawerNavigator />
+ 
 
-            </View>
-            
         )
     }
 }
--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -1,30 +1,59 @@
-import React from 'react'
+import React , { Component }from 'react'
 import {View , FlatList, Text, StyleSheet} from 'react-native'
 import ListItem, { Avatar } from 'react-native-elements'
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { DISHES } from '../shared/dishes';
 
-function Menu (props){
-    
-    
-    renderItem = ({ item, index }) => (
-       <TouchableOpacity style={styles.itemStyle} onPress = {() => props.onPress(item.id)}>
-          <View style = {styles.itemNameView} >
-           <Avatar size = {70} rounded source = {{ uri: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg'}} />
-           <Text style={[ styles.title ]}>{item.name}</Text>
-           </View>
-           <Text style={[ styles.desc ]}>{item.description}</Text>
-       </TouchableOpacity>
-);
 
+class Menu extends Component{
+    
+  constructor(props) {
+      super(props);
+      this.state  = {
+        dishes :  DISHES
+      };
+
+    }
+
+   
+
+
+render(){
+  const { navigate } = this.props.navigation;
+
+  const renderItem = ({ item, index }) =>   {
     return(
-        <FlatList 
-        data = {props.dishes}
-        renderItem={renderItem}
-        keyExtractor = {item => item.id.toString()}
-        />
-    )
-}
+        <TouchableOpacity style={styles.itemStyle} 
+        onPress={() => navigate('Dish Detail', { dishId: item.id })} 
+        data = {this.state.dishes}>
+            <View style = {styles.itemNameView} >
+              <Avatar size = {70} rounded source = {{ uri: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg'}} />
+              <Text style={[ styles.title ]}>{item.name}</Text>
+              </View>
+              <Text style={[ styles.desc ]}>{item.description}</Text>
+          </TouchableOpacity>
+    );
+  }
+  
+
+
+   return(
+
+     <FlatList 
+          data = {this.state.dishes}
+          renderItem={renderItem}
+          keyExtractor = {item => item.id.toString()}
+         />
+
+           )
+        }     
+      
+      }
+
+    
+    // )
+
 
 const styles = StyleSheet.create({
     container: {
